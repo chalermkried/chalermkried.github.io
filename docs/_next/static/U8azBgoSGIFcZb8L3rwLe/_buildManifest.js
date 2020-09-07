823a295413fb5320b60c623bf0b202bbd688bafa
Gear Chalermkried <chalermkried@gmail.com>
2020-09-08 00:20:29 +0700

    build(content): new

--- docs/_next/static/U8azBgoSGIFcZb8L3rwLe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-ca6a159bf32dc3053c8e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-83d9a4eab7fa36c1812c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
