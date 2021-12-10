f2be70233e71104d935186f93b32376d9ede5962
Gear Chalermkried <chalermkried@users.noreply.github.com>
2021-12-10 23:28:51 +0700

    feat: update content (#2)

--- docs/_next/static/h1Do1dMEHeJnPXmqqMFWy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-32ed8b9de16e194d28aa.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d203cc6bdebd357b5058.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
